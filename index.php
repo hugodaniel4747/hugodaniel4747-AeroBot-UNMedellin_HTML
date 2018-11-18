7cee3201d9c9a89687d6034636573d62d36cb45a
root <hugodan4747@gmail.com>
2018-11-18 17:18:31 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+
+<style>
+body {background-color: limeGreen;}
+</style>
+
+<body>
+
+<h1>This is the first AeroponicBot website!</h1>
+
+<button onclick="myFunction()">Reload page</button>
+
+<script>
+function myFunction() {
+    location.reload();
+}
+</script>
+
+<br>
+
+<?php 
+echo date('Y-m-d H:i:s');
+?>
+
+<p><img src="AeroponicBot_Logo.png"></p>
+
+
+
+</body>
+
+</html>
